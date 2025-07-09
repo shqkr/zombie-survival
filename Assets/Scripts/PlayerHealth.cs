7c8a1246a92824c05ad3492c2ec3751b312b7702
shqkr <seokhwan0611@gmail.com>
2025-07-09 17:12:10 +0900

    feat(player): add HP UI and HP mechanics

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -15,34 +15,82 @@ public class PlayerHealth : LivingEntity {
     private PlayerMovement playerMovement; // 플레이어 움직임 컴포넌트
     private PlayerShooter playerShooter; // 플레이어 슈터 컴포넌트
 
-    private void Awake() {
+    private void Awake()
+    {
         // 사용할 컴포넌트를 가져오기
+        playerAudioPlayer = GetComponent<AudioSource>();
+        playerAnimator = GetComponent<Animator>();
+        playerMovement = GetComponent<PlayerMovement>();
+        playerShooter = GetComponent<PlayerShooter>();
     }
 
-    protected override void OnEnable() {
+    protected override void OnEnable()
+    {
         // LivingEntity의 OnEnable() 실행 (상태 초기화)
         base.OnEnable();
+
+        healthSlider.gameObject.SetActive(true);
+        healthSlider.maxValue = startingHealth;
+        healthSlider.value = health;
+
+        playerMovement.enabled = true;
+        playerShooter.enabled = true;
     }
 
     // 체력 회복
-    public override void RestoreHealth(float newHealth) {
+    public override void RestoreHealth(float newHealth)
+    {
         // LivingEntity의 RestoreHealth() 실행 (체력 증가)
         base.RestoreHealth(newHealth);
+
+        // 갱신된 체력으로 체력 슬라이더 갱신
+        healthSlider.value = health;
     }
 
     // 데미지 처리
-    public override void OnDamage(float damage, Vector3 hitPoint, Vector3 hitDirection) {
+    public override void OnDamage(float damage, Vector3 hitPoint, Vector3 hitDirection)
+    {
+        if (!dead)
+        {
+            playerAudioPlayer.PlayOneShot(hitClip);
+        }
         // LivingEntity의 OnDamage() 실행(데미지 적용)
         base.OnDamage(damage, hitPoint, hitDirection);
+        // 갱신된 체력으로 체력 슬라이더 갱신
+        healthSlider.value = health;
     }
 
     // 사망 처리
-    public override void Die() {
+    public override void Die()
+    {
         // LivingEntity의 Die() 실행(사망 적용)
         base.Die();
+
+        // 체력 슬라이더 비활성화
+        healthSlider.gameObject.SetActive(false);
+        
+        // 사망음 재생
+        playerAudioPlayer.PlayOneShot(deathClip);
+        // 애니메이터의 Die 트리거를 발동시켜 사망 애니메이션 재생
+        playerAnimator.SetTrigger("Die");
+
+        // 플레이어 조작을 받는 컴포넌트 비활성화
+        playerMovement.enabled = false;
+        playerShooter.enabled = false;
     }
 
-    private void OnTriggerEnter(Collider other) {
+    private void OnTriggerEnter(Collider other)
+    {
         // 아이템과 충돌한 경우 해당 아이템을 사용하는 처리
+        if (!dead)
+        {
+            IItem item = other.GetComponent<IItem>();
+
+            if (item != null)
+            {
+                item.Use(gameObject);
+                playerAudioPlayer.PlayOneShot(itemPickupClip);
+            }
+        }
     }
 }
